a7e13bc23b5b6c491c1b8396306e4088807463bf
jugg <artjoms.vohmincevs@gmail.com>
2014-03-23 13:38:00 +0000

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='scrapyd_jongleur',
+      version='1.0',
+      description='Application for Scrapyd',
+      author='Artjoms Vohmincevs',
+      author_email='artjoms.vohmincevs@gmail.com',
+      packages=find_packages()
+     )	
